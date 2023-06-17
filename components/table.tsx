a5aae3d4f70d724f4a83399418a50ea085f2e69c
Shekey <ajdin_sah@hotmail.com>
2023-06-18 00:52:50 +0200

    prepare admin page for next stage

--- components/table.tsx
+++ components/table.tsx
@@ -1,24 +1,35 @@
+"use client";
+
 import {
-    Table,
-    TableHead,
-    TableRow,
-    TableHeaderCell,
-    TableBody,
-    TableCell,
-    Text
-  } from '@tremor/react';
-  
-  interface User {
-    id: number;
-    name: string;
-    email: string;
-    phone: string;
-    service: string;
-    problem: string;
-  }
-  
-  export default function UsersTable({ users }: { users: User[] }) {
-    return (
+  Table,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  TableBody,
+  TableCell,
+  Text,
+} from "@tremor/react";
+
+import { useQuery } from "react-query";
+
+const fetchUsers = async () => {
+  const res = await fetch("/api/users");
+  return res.json();
+};
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  service: string;
+  problem: string;
+}
+
+export default function UsersTable() {
+  const { data, status } = useQuery<User[]>("users", fetchUsers);
+
+  return (
       <Table>
         <TableHead>
           <TableRow>
@@ -28,24 +39,25 @@ import {
           </TableRow>
         </TableHead>
         <TableBody>
-          {users.map((user) => (
-            <TableRow key={user.id}>
-              <TableCell>{user.name}</TableCell>
-              <TableCell>
-                <Text>{user.email}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{user.phone}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{user.service}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{user.problem}</Text>
-              </TableCell>
-            </TableRow>
-          ))}
+          {data &&
+            data.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>
+                  <Text>{user.email}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{user.phone}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{user.service}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text>{user.problem}</Text>
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
-    );
-  }
+  );
+}
