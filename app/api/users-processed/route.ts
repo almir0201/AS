2f8f8338b94f044048ed0d7af43eca0a65398cc3
Shekey <ajdin_sah@hotmail.com>
2023-07-07 23:54:22 +0200

    added email sending

--- app/api/users-processed/route.ts
+++ app/api/users-processed/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-
+import { sendEmail } from "@/app/lib/email";
+import ApprovedAppointment from "@/app/emails/ApprovedAppointment";
+import DeclinedAppointment from "@/app/emails/DeclinedAppointment";
+import { render } from "@react-email/render";
 
 export async function POST(req: Request) {
   const { id, processed } = await req.json();
@@ -15,6 +18,13 @@ export async function POST(req: Request) {
         id
       },
     })
+    if (deleteUser.email) {
+      sendEmail({
+        to: deleteUser.email,
+        subject: "Termin za popravku automobila je odbijen",
+        html: render(DeclinedAppointment()),
+      });
+    }
     return NextResponse.json(deleteUser);
   } else {
     const user = await prisma.user.update({
@@ -25,6 +35,14 @@ export async function POST(req: Request) {
         processed
       }
     })
+    if (user.email) {
+      sendEmail({
+        to: user.email,
+        subject: "Termin za popravku automobila je prihvacen",
+        html: render(ApprovedAppointment()),
+      });
+    }
+  
     return NextResponse.json(user);
   }
 }
